2ccb2ffeb63e02c3f735058e1ee8b2deff25c1c2
Ruan Odendaal <ruanodendaal@gmail.com>
2017-02-21 18:41:59 +0000

    First pass at solving 11 classes

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -12,9 +12,38 @@
 # Zero bottles of beer on the wall.
 #
 # Your program should not use ninety-nine output statements!
-# Design your program with a class named BeerSong whose initialize method 
+# Design your program with a class named BeerSong whose initialize method
 # receives a parameter indicating the number of bottles of beer initially on the wall.
 # If the parameter is less than zero, set the number of bottles to zero. Similarly,
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+
+  attr_accessor :bottles
+
+  def initialize(bottles)
+    if bottles < 0
+      bottles = 0
+    elsif bottles > 99
+      bottles = 99
+    end
+
+    @bottles = bottles
+  end
+
+  def print_song
+    while bottles > 0
+    	puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer."
+    	bottles = bottles - 1
+    	puts "Take one down and pass it around, #{bottles} bottles of beer on the wall."
+    	puts ''
+    	if bottles == 0
+    		puts 'No more bottles of beer on the wall, no more bottles of beer.'
+    		puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+    		break
+    	end
+    end
+  end
+end
