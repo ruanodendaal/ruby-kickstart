593c982a601682fdf1c9a7f8fd1fce12df156ed4
Ruan Odendaal <ruanodendaal@gmail.com>
2017-02-21 09:08:10 +0000

    First pass at solving

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -3,10 +3,18 @@
 
 # Write a program that reads in two integers typed on the keybaord
 # and outputs their sum, difference, and product
-# 
+#
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
-  # your code goes here
+  input = $stdin.gets.chomp
+  # split into seperate integers
+  split = input.split.map(&:to_i)
+  # perform calculations
+  sum = split[0] + split[1]
+  difference = (split[0] - split[1])
+  product = split[0] * split[1]
+  # return results
+  print "#{sum}\n#{difference}\n#{product}\n"
 end
