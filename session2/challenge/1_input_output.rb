c82c019b926b500e8f51da9684d54e54c5fa052d
Ruan Odendaal <ruanodendaal@gmail.com>
2017-02-21 09:09:58 +0000

    Refactored after reviewing solution

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -8,13 +8,9 @@
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
-  input = $stdin.gets.chomp
-  # split into seperate integers
-  split = input.split.map(&:to_i)
+  a, b = gets.split.map(&:to_i)
   # perform calculations
-  sum = split[0] + split[1]
-  difference = (split[0] - split[1])
-  product = split[0] * split[1]
-  # return results
-  print "#{sum}\n#{difference}\n#{product}\n"
+  puts a + b
+  puts a - b
+  puts a * b
 end
